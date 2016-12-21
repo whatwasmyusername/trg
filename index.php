da7dc80c866d130e12c2c26bed8825698afd66be
Joe Delly <joe@ipsocreative.com>
2016-12-21 16:16:47 +0000

    Added GA

--- index.php
+++ index.php
@@ -35,6 +35,18 @@ function selfURL()
     <link rel="stylesheet" href="css/main.css" type="text/css">
     
     <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    
+    <!-- Google Analytics -->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-89256909-1', 'auto');
+ga('send', 'pageview');
+</script>
+<!-- End Google Analytics -->
 </head>
 <?php
 $message = urlencode("Tis' the Season for Giving");
